bcdf63bb74de1955004883acba4bed1c86e111d3
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2023-07-12 15:56:59 -0400

    Update Bus.route.ts

--- backend/nodejs/src/routes/Bus.route.ts
+++ backend/nodejs/src/routes/Bus.route.ts
@@ -7,6 +7,7 @@ router.get('/get', busController.getAllBuses);
 router.get('/get/:id', busController.getBusById);
 router.post('/post', busController.createBus);
 router.put('/put/:id', busController.updateBus);
+router.patch('/patch/:id', busController.patchBus);
 router.delete('/delete/:id', busController.deleteBus);
 
 export default router;
